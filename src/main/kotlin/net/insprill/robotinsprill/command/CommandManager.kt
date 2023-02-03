8b47d36870774a5c4e767cea78065c739e9e8623
Pierce Thompson <insprill@gmail.com>
2023-02-02 22:41:13 -0500

    Don't add -dev suffix to commands
    Leftover from 2b295ad9

--- src/main/kotlin/net/insprill/robotinsprill/command/CommandManager.kt
+++ src/main/kotlin/net/insprill/robotinsprill/command/CommandManager.kt
@@ -26,16 +26,14 @@ class CommandManager(private val robot: RobotInsprill) {
     suspend fun registerCommands(sCommands: Array<SlashCommand>, mCommands: Array<MessageCommand>) {
         robot.kord.createGuildApplicationCommands(robot.config.guildId) {
             sCommands.forEach { command ->
-                val cmdName = "${command.name}-dev"
-                input(cmdName, command.description) { command.setup(this) }
-                slashCommands[cmdName] = command
+                input(command.name, command.description) { command.setup(this) }
+                slashCommands[command.name] = command
             }
             robot.logger.info("Registered ${sCommands.size} slash commands")
 
             mCommands.forEach { command ->
-                val cmdName = "${command.name}-dev"
-                message(cmdName) { command.setup(this) }
-                messageCommands[cmdName] = command
+                message(command.name) { command.setup(this) }
+                messageCommands[command.name] = command
             }
             robot.logger.info("Registered ${messageCommands.size} message commands")
         }.collect()
